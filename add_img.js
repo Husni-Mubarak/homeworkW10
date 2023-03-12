9508ce448358684bcad7e891971124cb928fe3d3
Husni-Mubarak <husnimubarak859@gmail.com>
2023-03-12 18:59:47 +0700

    Memperbaiki Endpoint upload

--- add_img.js
+++ add_img.js
@@ -8,31 +8,36 @@ const diskstorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "uploads"));
   },
+
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9) + path.extname(file.originalname);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.replace(/\.jpeg|\.jpg|\.png/gi, "") + "-" + uniqueSuffix + path.extname(file.originalname);
+    cb(null, filename);
   },
 });
 
 uploadRouter.use("uploads", express.static(path.join(__dirname, "uploads")));
 uploadRouter.post("/uploads/:id", multer({ storage: diskstorage }).single("image"), (req, res) => {
-  const file = req.file.path;
+  const file = req.file;
   const { id } = req.params;
   if (!file) {
-    res.status(400).json({ massage: "file uploaded" });
+    res.status(400).json({ massage: "no file is selected" });
   } else {
     const updateMovieUrl = `
-            UPDATE movies
+            UPDATE movies    
             SET photo = $1
             WHERE id = $2;
             `;
 
-    pool.query(updateMovieUrl, [file, id], (err, response) => {
+    const photoUrl = "http://localhost:3000/" + file.filename;
+    pool.query(updateMovieUrl, [photoUrl, id], (err, response) => {
       if (err) {
         throw err;
       } else {
         res.status(200).json({ massage: "file uploaded" });
       }
+
+      console.log(photoUrl);
     });
   }
 });
